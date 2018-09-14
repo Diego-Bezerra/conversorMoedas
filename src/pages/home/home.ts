b6bf4d81d01de43f05085c3248dc61ef70b6111c
Diego Bezerra <diego.bezerra21@gmail.com>
2018-09-13 22:53:51 -0300

    'commit'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Toast } from '@ionic-native/toast';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  unityValue: number = 0.0062;
+  value: number;
+  result: string;
+
+  constructor(public navCtrl: NavController, private toast: Toast) {
 
   }
 
+  validate(): boolean {
+    return this.unityValue != null && this.value != null;
+  }
+
+  calculate() {
+    this.result = "";
+    if (this.validate()) {
+      this.result = 'R$' + this.unityValue * this.value;
+    } else {
+      this.toast.show('Im a toast', '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        });
+    }
+  }
 }
